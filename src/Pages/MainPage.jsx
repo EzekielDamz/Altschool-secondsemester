77cb9dbfcbe02ec36578ef156aa9fdf1db9b88d8
damilare dolapo jimoh <ezekieldammy1@gmail.com>
2024-04-22 22:13:51 +0100

    deployment issues

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -13,7 +13,7 @@ const MainPage = () => {
 
   useEffect(() => {
     const octokit = new Octokit({
-      // auth: githubToken,
+      auth: githubToken,
     });
     octokit
       .request("GET /user/repos")
